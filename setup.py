dff4bc40cdf767c15678e9533478501756ed52ca
virajkhanna <68863014+virajkhanna@users.noreply.github.com>
2020-12-31 15:32:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ __project__ = "makeSignIn"
 __version__ = "1.0.0"
 __description__ = "a Python module to create a sign in page for your python project."
 __packages__ = ["makesignin"]
+__author__ = "Viraj Khanna"
+__author_email__ = "virajkhanna2811@gmail.com"
 setup(
     name = __project__,
     version = __version__,
     description = __description__,
     packages = __packages__,
+    author = __author__,
+    author_email = __author_email__,
 )
