ae9310cc6b88a764642dbbc6e595214f3bdf48a3
virajkhanna <68863014+virajkhanna@users.noreply.github.com>
2020-12-31 15:40:01 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ __description__ = "a Python module to create a sign in page for your python proj
 __packages__ = ["makesignin"]
 __author__ = "Viraj Khanna"
 __author_email__ = "virajkhanna2811@gmail.com"
+__classifiers__ = ["Development Status :: 4 - Beta", "Intended Audience :: Developers", "License :: OSI Approved :: Boost Software License 1.0 (BSL-1.0)","Programming Language :: Python :: 3.9", "Natural Language :: English", "Topic :: Software Development :: Libraries", "Typing :: Typed"]
 setup(
     name = __project__,
     version = __version__,
@@ -12,4 +13,5 @@ setup(
     packages = __packages__,
     author = __author__,
     author_email = __author_email__,
+    classifiers = __classifiers__,
 )
