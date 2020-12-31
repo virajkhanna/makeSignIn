957db4e0cf76997e0b18479948e1cdf3a64244b8
virajkhanna <68863014+virajkhanna@users.noreply.github.com>
2020-12-31 15:29:53 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+__project__ = "makeSignIn"
+__version__ = "1.0.0"
+__description__ = "a Python module to create a sign in page for your python project."
+__packages__ = ["makesignin"]
+setup(
+    name = __project__,
+    version = __version__,
+    description = __description__,
+    packages = __packages__,
+)
