49f1e9f823e36da3093115ec640bbc3698db38fd
virajkhanna <68863014+virajkhanna@users.noreply.github.com>
2021-01-08 15:06:31 +0530

    Update me.py

--- makesignin/me.py
+++ makesignin/me.py
@@ -1,16 +1,17 @@
-def sign_in(user, passw):
+import getpass
+def sign_in(username_for_user, password_of_user):
   username = input("Enter your username: ")
-  if username != user:
+  if username != username_for_user:
     print ("Wrong username")
     return "Wrong Username, Try again"
   else:
     return "Correct Username"
-    password = input("Enter the password for this username: ")
-    if password != passw:
+    password = getpass.getpass("Enter the password for this username: ")
+    if password != password_of_user:
       print("Wrong Password")
       return "Wrong Password, Try again"
     else:
      return "Correct Password"
   if user == username and passw == password:
-    return "Sign In Successful"
+    return "1"
     print("Successful Sign In")
