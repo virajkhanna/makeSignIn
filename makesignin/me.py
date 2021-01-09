324cbddfff180e8468c9ab7e18cd9fdc2cf69797
virajkhanna <68863014+virajkhanna@users.noreply.github.com>
2021-01-09 12:18:48 +0530

    Update me.py

--- makesignin/me.py
+++ makesignin/me.py
@@ -1,4 +1,5 @@
 import getpass
+import win10toast
 def sign_in(username_for_user, password_of_user):
   username = input("Enter your username: ")
   if username != username_for_user:
@@ -15,3 +16,9 @@ def sign_in(username_for_user, password_of_user):
   if user == username and passw == password:
     return "1"
     print("Successful Sign In")
+    notify = win10toast.ToastNotifier()
+    notify.show_toast("Sign In", "Successful sign in", duration=7)
+    
+def get_username():
+  username1 = getpass.getuser()
+  return username1
