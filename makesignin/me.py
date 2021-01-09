a0e64847d59a6881750cd7d4b6f6f7e83dfee502
virajkhanna <68863014+virajkhanna@users.noreply.github.com>
2021-01-09 13:22:58 +0530

    Update me.py to fix errors and add features

--- makesignin/me.py
+++ makesignin/me.py
@@ -5,20 +5,21 @@ def sign_in(username_for_user, password_of_user):
   if username != username_for_user:
     print ("Wrong username")
     return "Wrong Username, Try again"
+    return 0
   else:
-    return "Correct Username"
     password = getpass.getpass("Enter the password for this username: ")
     if password != password_of_user:
       print("Wrong Password")
-      return "Wrong Password, Try again"
+      return "Wrong Password, Try again" 
+      return 0
     else:
-     return "Correct Password"
-  if user == username and passw == password:
-    return "1"
-    print("Successful Sign In")
-    notify = win10toast.ToastNotifier()
-    notify.show_toast("Sign In", "Successful sign in", duration=7)
+      if username_for_user == username and password_of_user == password:
+        return 1
+        print("Successful Sign In")
+        notify = win10toast.ToastNotifier()
+        notify.show_toast("Sign In", "Successful sign in", duration=7)
     
 def get_username():
   username1 = getpass.getuser()
   return username1
+
